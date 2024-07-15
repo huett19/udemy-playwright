d2395e56ff930b5dc4b7e5b19811976a75dac3be
huett <huett@vmogroup.com>
2024-07-15 17:32:45 +0700

    Add new file

---
+++ tests/example.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test'
+
+test('Simple basic test', async ({ page }) => {
+    //
+    await page.goto('https://example.com/')
+    const pageTitle = await page.locator('h1')
+    await expect(pageTitle).toContainText('Example Domain')
+})
